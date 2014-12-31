7a24e93d8c04a4fc824e1fc7d87bce5ef877c2eb
dherne <dherne@providenttechnologies.com>
2014-12-30 23:10:24 -0500

    Initial load of Contoso

---
+++ Modules.sql
@@ -0,0 +1,185 @@
+moz.Cstmer_Customer
+moz.Product_Product
+moz.Project_Budget
+moz.Project_project_type
+org.Company
+org.Department
+fin.GeneralLedger
+fin.Accounts
+fin.Assets
+fin.BankStatement
+crm.Leads
+crm.Sales
+crm.Quotes
+inv.Parts
+inv.Equipment
+inv.Route
+tim.Timesheet
+hr.Employee
+per.Employee
+gen.Address
+gen.Person
+
+
+
+Project.Project
+Project.ProjectBudget
+Project.Account
+
+Organization.Company
+Organization.BusinessUnit
+Organization.Hierarchy
+Organization.Calendar
+
+
+Products.Product
+Product.Service
+
+
+
+	User Permissions
+	Time Periods
+	Work Calendars
+	Holidays
+	Rate Cards
+	Expense Limits / Rates
+
+
+School.Class
+School.Teacher
+School.Schedule
+School.Student
+School.Administrator
+
+
+General.Person
+General.Address
+General.Company
+General.Code
+
+Customer.Cusomer
+Customer.Contact
+
+
+
+create table Project.Project
+
+
+--create schema mozaics.Customer
+/*
+
+-- RETURN
+
+MENUS:
+PROJECT - Projects / Time / Expense / Invoicing / Quote / Reports / Manage
+
+
+MODULE, FORM, LIST, GRID
+CreatedBy, CreatedOn, ModifiedBy, ModifiedOn
+REFRESH, NEW, FILTER, DELETE, SELECT COLUMNS (SAVE VIEW?), SORT, SAVE
+Configuration
+Key Performance Indicators
+
+Agile / Scrum
+ACTV Activity     - Login, Updates
+
+eCommerce    - 
+BNK - Finance      - Payments, Invoice, Accounts, Assets, Expense, Bank Statements
+FIN - Finance      - General Ledger, Chart of Account, Chart of CostCenter
+INV - Inventory     - parts, equipment, equip usage, part evel
+
+CMR - CRM   -  Leads, Opportunities, Quotes, Calls, Pipeline
+SLS - Sales - SalesOrder, Contracts, Sales Forecast
+
+ORG - Organization - company, department, facility, business unit, business stream, Regions, Practice
+
+PRD - Production   - Production Order,  Shipping, BOM, Work Center, Work Order
+
+PER - Personnel/HR - Person, Technician, Skills, Certifications, Recruitment, Timesheets, Leave Management
+PRJ - Project      - Contacts, Clients, Project, Tasks, Issues, Milestones, Planning, Budgeting, Incident, Invoicing
+PRG - Program
+PUR - Purchase     - Procurement, PurchaseOrders, Products, Quotes
+RES - Resource Mgmt- Team Scheduling, Utilization
+SVC - Service
+TST - Test         - test request
+TIM - TimeExpense
+
+CRM - Customers, Prospects
+BIL - Billing, Sales Orders, Invoicing
+
+REC - Sales, Invoicing
+PAY - Payables     - Suppliers, Supplier Invoice, Payments
+MKT - Marketing    - Lead Automation, Marketing Campaigns, Events, Surveys
+
+Warehouse
+
+
+ServiceDesk - Incident, Proglem, Change, SLA, Knowledge Base, Assets
+Marketing
+Billing
+Expnese
+
+Project Portfolio Management
+Project Management
+Project Accounting
+Budgeting  (multiple??)
+Forecasting  (multiple??)
+
+
+Project Budgeting
+Project Collaboration
+Project Costing
+Project Expense Management
+Project Issue Tracking
+Project Planning
+Project Progress Tracking
+Project Risk Management
+Project Scheduling
+Project Status Reporting
+Project Task Management
+Project Time Billing
+Project Invoicing ---
+Resource Scheduling
+Resource Utilization
+Team Collaboration
+Timesheet Management
+
+Project:
+	Contacts
+	Budgets
+	Milestones
+	Locations
+	Phases
+	Resource Requests
+	Resource Assignments
+	timecards
+	Expenses
+	Notes
+	Attachments
+	Content Deliveries
+	Project Materials
+	Billing Events
+	Revenue Recognized Events
+	Est V. Actuals
+	Sales Invoices
+	Cases
+	Issues / Risks / Decisions
+	Status Reports
+	Open Activities
+	Surveys
+	Project Hisotry
+
+Timecard:
+	User Permissions
+	Time Periods
+	Work Calendars
+	Holidays
+	Rate Cards
+	Expense Limits / Rates
+
+NonProfit
+	Donations
+
+School
+	
+*/
---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+--create schema mozaics.Customer
+/*
+Bank
+Cash
+Customer     -
+CRM
+Finance      - Payments, Invoice, Accounts
+Inventry     - parts, equipment, equip usage, part evel
+Organization - company, department, facility, business unit, business stream
+Production   - Production Order,
+Personnel - Person, Technician
+Projects
+Purchasing   -
+Service
+Test         - test request
+TimeExpense
+
+Finance
+Receivables  - Sales
+Payables
+Marketing
+
+*/
